797c91ab1f3b415f4f866a9971089df914b02688
Majd Barakat <54665027+majdbrt@users.noreply.github.com>
2023-05-07 23:39:20 -0500

    added Spotify API integration

--- client/store/useStore.js
+++ client/store/useStore.js
@@ -3,10 +3,25 @@ import { create } from 'zustand'
 const useStore = create((set) => ({
   windowYScroll: 0,
   setWindowYScroll: (y) => set(() => ({ windowYScroll: y })),
-  overViewColor: [30,30,30],
-  setOverViewColor: (color) => set(()=>({
+  overViewColor: [30, 30, 30],
+  setOverViewColor: (color) => set(() => ({
     overViewColor: color
-  }))
+  })),
+  profileLoading: true,
+  setProfileLoading: () => set(() => ({ profileLoading: false })),
+  albumsLoading: true,
+  setAlbumsLoading: () => set(() => ({ albumsLoading: false })),
+  featuredPlaylistsLoading: true,
+  setFeaturedPlaylistsLoading: () => set(() => ({ featuredPlaylistsLoading: false })),
+  topArtistsLoading: true,
+  setTopArtistsLoading: () => set(() => ({ topArtistsLoading: false })),
+  playlistsLoading: true,
+  setPlaylistsLoading: () => set(() => ({ playlistsLoading: false })),
+  headerProfileElement: null,
+  setHeaderProfileElement: (elem) => set(() => ({ headerProfileElement: elem })),
+  queue:[],
+  setQueue: (trackArr) => set(() => ({ queue: trackArr }))
+
 }))
 
 export default useStore;
